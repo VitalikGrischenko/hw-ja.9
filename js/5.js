9f8d9e9c240f552652f3caf675b20137db304635
VitalikGrischenko <110487381+VitalikGrischenko@users.noreply.github.com>
2023-04-27 19:19:12 +0300

    1245

--- js/5.js
+++ js/5.js
@@ -1,8 +1,11 @@
-const arrayStr = ["deyrey", "fgkktkryjity", "sarysery", "ertery", "eyry"];
-let arrayStrLenght;
-for (let i = 1; i <= 5; i += 1) {
-    arrayStrLenght = arrayStr[i].length;
-    if (arrayStrLenght >= 5) {
-        console.log(arrayStr[i]);
+const arruyStr = ["deyrey", "fgkktkryjity", "sarysery", "ertery", "eyry"];
+let arruyStrLenght;
+let ulementys;
+for (let i = 1; i < arruyStr.length; i += 1) {
+    ulementys = arruyStr[i];
+    console.log(ulementys);
+    arruyStrLenght = ulementys.length;
+    if (arruyStrLenght >= 5) {
+        console.log(ulementys);
     }
 };
